ee273e0e3dcf3af24f2489f1535bb57df90f60b6
David Lewitan <dovidlwtn@gmail.com>
2017-02-09 18:00:56 +0000

    will it work?

---
+++ lib/dovidsgem.rb
@@ -0,0 +1,11 @@
+require_relative "./dovidsgem/version"
+
+module Dovidsgem
+  class Cli
+    def self.run
+      puts "hi Dovid"
+    end
+  end
+end
+
+Dovidsgem::Cli.run
---
+++ lib/dovidsgem/version.rb
@@ -0,0 +1,3 @@
+module Dovidsgem
+  VERSION = "0.1.0"
+end
